e5c19a85f9b6546f9e8e915eb71e7591d0b6b5df
Schimi42 <michell.schimanski@gmail.com>
2026-04-19 19:51:46 +0200

    Initial Commit

---
+++ Negative Converter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Negative Converter
+//
+//  Created by Michell Schimanski on 19.04.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Negative Converter/Negative_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Negative_ConverterApp.swift
+//  Negative Converter
+//
+//  Created by Michell Schimanski on 19.04.26.
+//
+
+import SwiftUI
+
+@main
+struct Negative_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
